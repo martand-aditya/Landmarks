29bb39b6fb2cd49eb7e119752c42f4a05a6db859
martand-aditya <martand.aditya@gmail.com>
2024-04-01 14:26:19 +0530

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Martandaditya on 01/04/24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
